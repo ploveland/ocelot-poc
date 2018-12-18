c0366e2ff404550c7f3dacc93478c94cf07f5994
Pam Loveland <ploveland@reliaslearning.com>
2018-12-18 15:49:56 -0500

    first commit

---
+++ Authentication/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Authentication.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ Catalog/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Catalog.Models
+{
+    public class Product
+    {
+        public Guid ProductId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
